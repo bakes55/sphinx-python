9b2bf1f13ffb091e7ef0a52b2cfad58af7e2f18b
Garry Baker <trading@chillinet.com.au>
2019-07-16 15:51:08 +1000

    adding functions to document

---
+++ example1.py
@@ -0,0 +1,18 @@
+def funct1(arg1, arg2):
+    """
+    this is a test function. takes 2 arguments, sets first to equal second.
+    :param arg1: some value
+    :param arg2: another value
+    :return: arg1
+    """
+    arg1 = arg2
+
+    return arg1
+
+def func2():
+    """
+    Function with no arguments, return None
+
+    :return: None
+    """
+    return None
